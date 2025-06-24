77417b211a988ae28bc6d9e2ee299234d9b00e95
John <jjckrbbt@gmail.com>
2025-06-24 08:10:31 -0400

    changes and more workflow

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,14 +1,14 @@
 package auth
 
 import (
-	"testing"
 	"net/http/httptest"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
 	reqValid := httptest.NewRequest("GET", "/some/api/route", nil)
 	reqValid.Header.Set("Authorization", "ApiKey valid-api-key")
-	
+
 	apiKey, err := GetAPIKey(reqValid.Header)
 	if err != nil {
 		t.Errorf("handler err: %v", err)
@@ -20,7 +20,7 @@ func TestGetAPIKey(t *testing.T) {
 
 	reqInvalid := httptest.NewRequest("GET", "/some/api/route", nil)
 	reqInvalid.Header.Set("Authorization", "apikey invalid-api-key")
-	
+
 	apiKey, err = GetAPIKey(reqInvalid.Header)
 	if err == nil {
 		t.Errorf("handler did not err: %v", err)
@@ -32,7 +32,7 @@ func TestGetAPIKey(t *testing.T) {
 
 	reqNoAuthHeader := httptest.NewRequest("GET", "/some/api/route", nil)
 	reqNoAuthHeader.Header.Set("NotAnAuthorization", "ApiKey no-auth-header")
-	
+
 	_, err = GetAPIKey(reqNoAuthHeader.Header)
 	if err == nil {
 		t.Errorf("handler returned validated key in error")
